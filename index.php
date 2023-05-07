6b4ea252aa6073c0153b6771439b6785b29437d2
Apli04 <aplianagallu1@gmail.com>
2023-05-07 18:46:34 +0700

    FSWD1_PHP3

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php 
+require 'connect.php';
+//List product dari database
+$sql = 'SELECT * FROM product';
+$result = mysqli_query($con, $sql);
+ ?>
+<h2> Select the items: </h2>
+ <table id="t01">
+ <tr>
+     <th>Id</th>
+     <th>Name</th>
+     <th>Price</th>
+     <th>Quantity (in stock)</th>
+     <th>Buy</th>
+ </tr>
+     <?php while($product = mysqli_fetch_object($result)) { ?> 
+    <tr>
+        <td> <?php echo $product->id; ?> </td>
+        <td> <?php echo $product->name; ?> </td>
+        <td> Rp.<?php echo $product->price; ?> </td>
+        <td> <?php echo $product->quantity; ?> </td>
+        <td> <a href="cart.php?id= <?php echo $product->id; ?> &action=add">Order Now</a> </td>
+    </tr>
+    <?php } ?>
+ </table>
+</body>
+
+ </html>
+5 Membuat file item.php
+<?php 
+class Item{
+ var $id;//var int
+ //var string
+ var $name;
+ //var float
+ var $price;
+ //var int
+ var $quantity;
+}
+ ?>
